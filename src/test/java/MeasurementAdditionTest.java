f112790f4c1886c8e495ff022855f61ed03f76cb
santoshingale <santoshingale876@gmail.com>
2020-01-28 17:55:00 +0530

    Added : Test case which check Addition of Feet and Feet value and return it in Inch

--- src/test/java/MeasurementAdditionTest.java
+++ src/test/java/MeasurementAdditionTest.java
@@ -20,4 +20,20 @@ public class MeasurementAdditionTest {
         double totalValue = first.getAddition(second);
         Assert.assertEquals(14, totalValue, 0);
     }
+
+    @Test
+    public void given1FeetAnd1Fet_shouldReturnAdditionInInch() {
+        QuantityMeasurement first = new QuantityMeasurement(Unit.FEET, 1);
+        QuantityMeasurement second = new QuantityMeasurement(Unit.FEET, 1);
+        double totalValue = first.getAddition(second);
+        Assert.assertEquals(24, totalValue, 0);
+    }
+
+    @Test
+    public void given2InchAndCentimeter_shouldReturnAdditionInInch() {
+        QuantityMeasurement first = new QuantityMeasurement(Unit.INCH, 2);
+        QuantityMeasurement second = new QuantityMeasurement(Unit.CM, 2.5);
+        double totalValue = first.getAddition(second);
+        Assert.assertEquals(3, totalValue, 0);
+    }
 }
