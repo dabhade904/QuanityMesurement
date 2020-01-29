417a8f23f9f9ae0d4f939c6adc243c2196c9af1d
santoshingale <santoshingale876@gmail.com>
2020-01-29 17:29:41 +0530

    Added : Funtionality to sum of Liters and Milliliter

--- src/test/java/MeasurementAdditionTest.java
+++ src/test/java/MeasurementAdditionTest.java
@@ -38,10 +38,18 @@ public class MeasurementAdditionTest {
     }
 
     @Test
-    public void given1GallonAnd3Liter780MiliLiter_shouldReturnAdditionInLiter() {
+    public void given1GallonAnd3Liter780MilliLiter_shouldReturnAdditionInLiter() {
         QuantityMeasurement first = new QuantityMeasurement(Unit.GALLON, 1);
         QuantityMeasurement second = new QuantityMeasurement(Unit.LITER, 3.78);
         double totalValue = first.getAddition(second);
         Assert.assertEquals(7.56, totalValue, 0);
+    }
+
+    @Test
+    public void given1LiterAnd1000MilliLiter_shouldReturnAdditionInLiter() {
+        QuantityMeasurement first = new QuantityMeasurement(Unit.LITER, 1);
+        QuantityMeasurement second = new QuantityMeasurement(Unit.ML, 1000);
+        double totalValue = first.getAddition(second);
+        Assert.assertEquals(2, totalValue, 0);
     }
 }
